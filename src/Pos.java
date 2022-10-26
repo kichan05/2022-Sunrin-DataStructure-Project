599330f97bd93f355f3b301eed244f9bca01adeb
Heechan <ckstmznf@naver.com>
2022-10-26 09:12:39 +0900

    add:Pos method

--- src/Pos.java
+++ src/Pos.java
@@ -1,8 +1,29 @@
 public class Pos {
-    int x;
-    int y;
+    private int x;
+    private int y;
 
     Pos(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
+
+    public void setX(int x){
+        this.x = x;
+    }
+
+    public void setY(int y){
+        this.y = y;
+    }
+
+    public void setPos(int x, int y){
         this.x = x;
         this.y = y;
     }

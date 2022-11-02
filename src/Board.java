baff66570dd190aec1da5813209ce02422b76cb8
Laptop <Laptop@KJH>
2022-11-02 09:31:27 +0900

    Add : playground

--- src/Board.java
+++ src/Board.java
@@ -6,14 +6,32 @@ import java.util.ArrayList;
 
 public class Board {
 
-    private ArrayList<Piece> pieces = new ArrayList<Piece>();
+    private ArrayList<Piece> pieces = new ArrayList<>();
+    private char[][] playground
+            = {{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}};
 
     public void printBoard() {
+        for (int i = 0; i < pieces.size(); i++){
+            int pieceX = pieces.get(i).getPosX();
+            int pieceY = pieces.get(i).getPosY();
+
+            playground[pieceX][pieceY] = '1';
+        }
+
         System.out.print("\n\n\n\n\n\n\n\n\n\n\n\n\n");
 
         System.out.println("====================");
-        for (int i = 0; i < pieces.size(); i++){
-            System.out.printf("%d, %d, %c\n", pieces.get(i).getPosX(), pieces.get(i).getPosY(), pieces.get(i).getShape());
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+
+            }
         }
         System.out.println("====================");
     }
@@ -28,4 +46,5 @@ public class Board {
     public void clearBoard() {
         pieces.clear();
     }
+
 }

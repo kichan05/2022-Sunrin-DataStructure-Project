4340e7e784b3fb28bb08074e9fa8e2b198378f9b
Laptop <Laptop@KJH>
2022-11-03 13:41:11 +0900

    Hello December

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
+        boolean gameState = true;
+
         Board board = new Board();
 
         board.initGameBoard();
 
-        while (true){
+        while (gameState){
             break;
         }
 

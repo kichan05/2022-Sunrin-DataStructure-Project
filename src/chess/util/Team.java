1a40ed0affb00186cfcaf3e3b2e8be2f90a55674
Heechan <ckstmznf@naver.com>
2022-11-26 00:04:08 +0900

    Team color

--- src/chess/util/Team.java
+++ src/chess/util/Team.java
@@ -2,4 +2,12 @@ package chess.util;
 
 public enum Team {
     RED, BLUE;
+
+    public String getTeamColor() {
+        if (this == Team.BLUE) {
+            return Color.BLUE.getFontColor();
+        } else {
+            return Color.RED.getFontColor();
+        }
+    }
 }

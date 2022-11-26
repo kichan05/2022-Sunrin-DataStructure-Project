c8d38dcf8f87b5f673f6429bbcbac55d47401c75
Heechan <ckstmznf@naver.com>
2022-11-26 16:04:52 +0900

    King none death

--- src/chess/player/Human.java
+++ src/chess/player/Human.java
@@ -2,9 +2,12 @@ package chess.player;
 
 import chess.Board;
 import chess.ChessUi;
+import chess.pieces.King;
 import chess.pieces.Piece;
 import chess.util.Pos;
 import chess.util.Team;
+
+import javax.xml.crypto.dsig.keyinfo.KeyInfo;
 
 public class Human extends Player {
     public Human(Team team) {
@@ -42,12 +45,14 @@ public class Human extends Player {
         /* 선택한 기물 이동 */
         while (true) {
             Pos inputPos = inputPos("기물을 이동할 위치를 선택하세요 : ");
-
+            Piece targetPiece = Board.board.getPieceByPos(inputPos);
 
             if (!inputPos.inBoard()) {
                 ChessUi.printErrorMessage("범위를 벗어난 입력입니다.");
             } else if (!selectPiece.canMove(inputPos)) {
                 ChessUi.printErrorMessage("해당 위치로 이동할 수 없습니다.");
+            } else if (targetPiece instanceof King) {
+                ChessUi.printErrorMessage("King은 잡을 수 없습니다.");
             } else {
                 return inputPos;
             }
